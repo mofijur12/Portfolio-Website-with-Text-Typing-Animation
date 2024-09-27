8ad5f149ab609b28f71d34fffaac0b7a2b5a8bcc
mofijur12 <mrkhan55679@gmail.com>
2024-09-27 22:33:11 +0600

    Portfolio Website With Text Typing Animation

---
+++ app.js
@@ -0,0 +1,7 @@
+let typed = new typed(".type-text",{
+    string:["Elon Musk" ,"CEO of SpaceX","CEO of Tesla Motors"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
